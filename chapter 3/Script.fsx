aa7aa3d920bcac6739a4bd5634cefe29a6662fb6
Karlkim Suwamongkol <karlkim@outlook.com>
2014-06-03 21:15:47 -0500

    Finish Ex. 2 Add support for hard line breaks

--- chapter 3/Script.fsx
+++ chapter 3/Script.fsx
@@ -18,6 +18,7 @@ and MarkdownSpan =
 | Strong of MarkdownSpans   
 | Emphasis of MarkdownSpans   
 | HyperLink of MarkdownSpans * string
+| HardLineBreak
 
 let rec parseInlineBody acc = function   
     | '`'::rest ->        
@@ -36,18 +37,102 @@ let parseInline = function
 
 let toString chars = 
     System.String(chars |> Array.ofList)  
+//
+//let rec parseSpans acc chars = seq {
+//    let emitLiteral = seq {
+//        if acc <> [] then
+//            yield acc |> List.rev |> toString |> Literal }    
+//            
+//    match parseInline chars, chars with
+//    | Some(body, chars), _ -> 
+//        yield! emitLiteral
+//        yield body |> toString |> InlineCode
+//        yield! parseSpans [] chars   
+//    | _, c::chars ->
+//        yield! parseSpans (c::acc) chars
+//    | _, [] ->
+//        yield! emitLiteral     
+//    }
+
+
+
+
+
+let (|StartsWith|_|) prefix input =
+    let rec loop = function
+        | p::prefix, r::rest when p = r ->
+            loop(prefix, rest)
+        | [], rest ->
+            Some(rest)
+        | _ -> None
+    loop(prefix, input)
+
+let rec parseBracketedBody closing acc = function
+    | StartsWith closing (rest) ->
+        Some(List.rev acc, rest)
+    | c::chars ->
+        parseBracketedBody closing (c::acc) chars
+    | _ -> None
+
+let parseBracketed opening closing = function
+    | StartsWith opening chars ->
+        parseBracketedBody closing [] chars
+    | _ -> None
+
+let (|Delimited|_|) delim = parseBracketed delim delim
+
+let (|Bracketed|_|) opening closing = parseBracketed opening closing
 
 let rec parseSpans acc chars = seq {
     let emitLiteral = seq {
         if acc <> [] then
             yield acc |> List.rev |> toString |> Literal }    
-            
-    match parseInline chars, chars with
-    | Some(body, chars), _ -> 
+    
+    match chars with
+    | Delimited ['`'] (body, chars) ->
         yield! emitLiteral
         yield body |> toString |> InlineCode
-        yield! parseSpans [] chars   
-    | _, c::chars ->
+        yield! parseSpans [] chars
+    | Delimited ['*';'*'] (body, chars)
+    | Delimited ['_';'_'] (body, chars) ->
+        yield! emitLiteral
+        yield Strong(parseSpans [] body |> List.ofSeq)
+        yield! parseSpans [] chars
+    | Delimited ['*'] (body, chars)
+    | Delimited ['_'] (body, chars) ->
+        yield! emitLiteral
+        yield Emphasis(parseSpans [] body |> List.ofSeq)
+        yield! parseSpans [] chars
+    // Exercise 1: Add parsing of hyperlinks
+    | Bracketed ['['] [']'] (body, chars) ->        
+        match chars with
+        | Bracketed ['('] [')'] (url, chars) ->
+            yield! emitLiteral
+            yield HyperLink(parseSpans [] body |> List.ofSeq, url |> toString)
+            yield! parseSpans [] chars
+        | _ -> 
+            yield! emitLiteral
+            yield body |> toString |> sprintf "[%s]" |> Literal
+            yield! parseSpans [] chars
+    // Exercise 2: Add support for hard line breaks
+    | StartsWith [' ';'\n';'\r'] chars 
+    | StartsWith [' ';'\r'] chars 
+    | StartsWith [' ';'\n'] chars ->
+        yield! emitLiteral
+        yield HardLineBreak
+        yield! parseSpans [] chars
+    | c::chars ->
         yield! parseSpans (c::acc) chars
-    | _, [] ->
-        yield! emitLiteral }
+    | [] ->
+        yield! emitLiteral     
+    }  
+
+
+"where **is** the `code` _**Hello**_" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
+"" |> List.ofSeq |> parseSpans []
+
+// Exercise 1: Add parsing of hyperlinks
+"For [more] information, see [**F#** home page](http://fsharp.net). __Thank you!__" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
+
+// Exercise 2: Add support for hard line breaks
+"hello \n\rworld \r!!!" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
