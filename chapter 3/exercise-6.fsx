883cfa114b7f0da606543aa3eb6abb0f6c24ca1c
Karlkim Suwamongkol <karlkim@outlook.com>
2014-06-15 22:03:02 -0500

    start exercise 6

--- chapter 3/exercise-6.fsx
+++ chapter 3/exercise-6.fsx
@@ -1,14 +1,7 @@
-﻿let txt = """Visual F#
-=========
-F# is a **programming language** that supports _functional_, as well as _object-oriented_ and _imperative_ programming styles. Hello world can be written as follows:
-
-    `printfn "Hello world!"` 
-
-For more information, see the [F# home page] (http://fsharp.net) or  read [Real-World Func tional Programming](http://manning.com/petricek)  published by [Manning](http://manning.com)."""
-
-type MarkdownDocument = list<MarkdownBlock>
+﻿type MarkdownDocument = list<MarkdownBlock>
 and MarkdownBlock =
-| Heading of int * MarkdownSpans   
+| Heading of int * MarkdownSpans
+| NumberHeading of int * MarkdownSpans
 | Paragraph of MarkdownSpans   
 | CodeBlock of list<string>
 | BlockQuote of list<MarkdownBlock>
@@ -22,36 +15,9 @@ and MarkdownSpan =
 | HyperLink of MarkdownSpans * string
 | HardLineBreak
 
-let rec parseInlineBody acc = function   
-    | '`'::rest ->        
-        Some(List.rev acc, rest)
-    | c::chars  ->        
-        parseInlineBody (c::acc) chars
-    | [] -> None
-    
-let parseInline = function   
-    |'`'::chars ->       
-        parseInlineBody [] chars   
-    | _ -> None   
-
 let toString chars = 
     System.String(chars |> Array.ofList)  
-//
-//let rec parseSpans acc chars = seq {
-//    let emitLiteral = seq {
-//        if acc <> [] then
-//            yield acc |> List.rev |> toString |> Literal }    
-//            
-//    match parseInline chars, chars with
-//    | Some(body, chars), _ -> 
-//        yield! emitLiteral
-//        yield body |> toString |> InlineCode
-//        yield! parseSpans [] chars   
-//    | _, c::chars ->
-//        yield! parseSpans (c::acc) chars
-//    | _, [] ->
-//        yield! emitLiteral     
-//    }
+
 
 let (|StartsWith|_|) prefix input =
     let rec loop = function
@@ -84,21 +50,25 @@ let rec parseSpans acc chars = seq {
             yield acc |> List.rev |> toString |> Literal }    
     
     match chars with
+    // `let x = 0`
     | Delimited ['`'] (body, chars) ->
         yield! emitLiteral
         yield body |> toString |> InlineCode
         yield! parseSpans [] chars
+    // **strong** __strong__
     | Delimited ['*';'*'] (body, chars)
     | Delimited ['_';'_'] (body, chars) ->
         yield! emitLiteral
         yield Strong(parseSpans [] body |> List.ofSeq)
         yield! parseSpans [] chars
+    // *emphasis* _emphasis_
     | Delimited ['*'] (body, chars)
     | Delimited ['_'] (body, chars) ->
         yield! emitLiteral
         yield Emphasis(parseSpans [] body |> List.ofSeq)
         yield! parseSpans [] chars
-    // Exercise 1: Add parsing of hyperlinks
+    // [link](http://link)
+    // or [literal]
     | Bracketed ['['] [']'] (body, chars) ->        
         match chars with
         | Bracketed ['('] [')'] (url, chars) ->
@@ -109,7 +79,6 @@ let rec parseSpans acc chars = seq {
             yield! emitLiteral
             yield body |> toString |> sprintf "[%s]" |> Literal
             yield! parseSpans [] chars
-    // Exercise 2: Add support for hard line breaks
     | StartsWith [' ';'\n';'\r'] chars 
     | StartsWith [' ';'\r'] chars 
     | StartsWith [' ';'\n'] chars ->
@@ -121,17 +90,6 @@ let rec parseSpans acc chars = seq {
     | [] ->
         yield! emitLiteral     
     }  
-
-
-"where **is** the `code` _**Hello**_" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
-"" |> List.ofSeq |> parseSpans []
-
-// Exercise 1: Add parsing of hyperlinks
-"For [more] information, see [**F#** home page](http://fsharp.net). __Thank you!__" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
-
-// Exercise 2: Add support for hard line breaks
-"hello \n\rworld \r!!!" |> List.ofSeq |> parseSpans [] |> Array.ofSeq
-
 
 module List = 
     let partitionWhile f = 
@@ -155,13 +113,6 @@ let (|LineSeparated|) lines =
 let (|AsCharList|) (str:string) =
     List.ofSeq str
 
-
-let (PrefixedLines "..." res) = ["...1";"...2";"3"]
-let (PrefixedLines "..." res) = ["1";"...2";"...3"]
-let (PrefixedLines "..." res) = ["...1";"2";"...3"]
-
-
-// Exercise 3 : Improve and complete parsing of headings
 let (|Heading'|_|) = function
     | AsCharList(StartsWith ['#'; ' '] heading)::lines -> Some(1,heading,lines) 
     | AsCharList(StartsWith ['#'; '#'; ' '] heading)::lines -> Some(2,heading,lines) 
@@ -174,16 +125,9 @@ let rec parseBlocks lines = seq {
     | Heading'(size, heading, lines) ->
         yield Heading(size, parseSpans [] heading |> List.ofSeq)
         yield! parseBlocks lines
-//    | AsCharList(StartsWith ['#'; ' '] heading)::lines ->
-//        yield Heading(1, parseSpans [] heading |> List.ofSeq)
-//        yield! parseBlocks lines
-//    | AsCharList(StartsWith ['#'; '#'; ' '] heading)::lines ->
-//        yield Heading(2, parseSpans [] heading |> List.ofSeq)
-//        yield! parseBlocks lines
     | PrefixedLines "    " (body, lines) when body <> [] ->
         yield CodeBlock(body)
         yield! parseBlocks lines  
-    // Exercise 4: Add support for block quotes
     | LineSeparated (body, lines) when body <> [] 
             && body.Head.StartsWith(">") 
             && (body |> List.filter(fun s -> s.StartsWith(">")) |> List.length) = 1 ->
@@ -204,72 +148,6 @@ let rec parseBlocks lines = seq {
     | _ -> ()
 }
 
-let sample = """# Introducing F#
-F# is a _functional-first_ language,
-which looks like this:
-
-    let msg = "world"
-    printfn "hello %s!" msg
-
-This sample **prints** `hello world!`
-
-## h2 heading
-
-h1 heading
-==========
-h2 heading
-----------
-No heading
-
-> This is a hand-written quotation
-which consists of two paragraphs.
-
-> The second paragraph is quite short.
-
-> This is a quotation that contains F# code:
-> 
->     printfn "Hello from quoted code!"
->     let msg = "world"
->     printfn "hello %s!" msg
-
-> Another *test*
-> More test
-
-"""
-
-let sampleDoc = sample.Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq
-
-txt.Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq
-
-let (PrefixedLines ">" res) = ["> This is a hand-written quotation";"which consists of two paragraphs."]
-let (PrefixedLines ">" res) = ["> Another *test*";"> More test"]
-
-
-"""
-> This is a hand-written quotation
-which consists of two paragraphs.
-""".Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq  
-
-"""
-> The second paragraph is quite short.
-""".Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq  
-
-"""
-> This is a quotation that contains F# code:
-> 
->     printfn "Hello from quoted code!"
->     let msg = "world"
->     printfn "hello %s!" msg
-""".Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq
-
-"""
-> Another *test*
-> More test
-""".Split('\r','\n') |> List.ofSeq |> parseBlocks |> List.ofSeq
-
-
-
-// Markdown to HTML
 open System.IO
 
 let outputElement (output:TextWriter) tag attributes body =
@@ -307,12 +185,6 @@ let rec formatBlock (output:TextWriter) block =
         out "pre" [] (fun() -> lines |> List.iter output.WriteLine)
     | BlockQuote(blocks) ->
         out "quote" [] (fun() -> blocks |> List.iter (formatBlock output))
-
-let sb = System.Text.StringBuilder()
-let output = new StringWriter(sb)
-
-sampleDoc |> List.iter (formatBlock output)
-sb.ToString()        
 
 module Matching = 
     let (|SpanNode|_|) span =
@@ -357,14 +229,6 @@ let generateBlockRefs refs = function
         Matching.BlockNode(shape, children)
     | block -> block
 
-let doc = 
-    [ """For **more** information, see the [F# home page](http://fsharp.net) or read [Real-World Functional Programming](http://manning.com/petricek) published by [Manning](http://manning.com)."""] 
-    |> parseBlocks |> List.ofSeq
-
-let refs = ResizeArray<_>()
-let printable = doc |> List.map (generateBlockRefs refs)
-
-// Exercise 5: Count the number of words
 open System.Text
 open System.Text.RegularExpressions
 
@@ -385,14 +249,13 @@ let countWordsInBlock count = function
         children |> List.iter (countWordsInSpan count)
     | _ -> ()
 
-doc |> List.iter (countWordsInBlock count)
-count := 0
-sampleDoc |> List.iter (countWordsInBlock count)
 
-txt.Split('\r','\n') 
+let headings =  """# Heading 1
+## Sub-heading 1
+## Sub-heading 2 
+# Heading 2"""
+
+headings.Split('\r','\n')
 |> List.ofSeq 
-|> parseBlocks 
+|> parseBlocks
 |> List.ofSeq
-|> List.iter (countWordsInBlock count) // 45 words
-
-
